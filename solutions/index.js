14599edf377a11466593e4d43158507ca6a7a69a
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2023-05-17 01:32:19 +0200

    added data, basic chart and filter markup

--- solutions/index.js
+++ solutions/index.js
@@ -1,3 +1,42 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
-const body = d3.select("#chart").append("p").text("Hello from D3");
+import { elections2021, partyColours } from "./data/data.js";
+
+let dataset = elections2021.sort((a, b) => b.result - a.result);
+
+const w = 800;
+const h = 400;
+
+const barPadding = 5;
+
+const xScale = d3
+  .scaleLinear()
+  .domain([0, d3.max(dataset, (d) => d.result)])
+  .range([0, w]);
+
+const svg = d3
+  .select("#chart")
+  .append("svg")
+  .attr("width", w)
+  .attr("height", h);
+
+const bars = svg
+  .selectAll("rect")
+  .data(dataset)
+  .enter()
+  .append("rect")
+  .attr("x", 0)
+  .attr("y", (d, i) => i * (h / dataset.length))
+  .attr("width", (d) => xScale(d.result))
+  .attr("height", h / dataset.length - barPadding)
+  .style("fill", (d) => partyColours.find((pC) => pC.party === d.party).colour);
+
+const labels = svg
+  .selectAll("text")
+  .data(dataset)
+  .enter()
+  .append("text")
+  .text((d) => `${d.party}: ${d.result}%`)
+  .attr("x", 5)
+  .attr("y", (d, i) => (i + 0.5) * (h / dataset.length))
+  .style("fill", "white");
